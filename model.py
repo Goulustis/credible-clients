3edbe42f967ece108298223d8b7b17907cfd3dca
Goulustis <43025723+Goulustis@users.noreply.github.com>
2018-09-08 04:45:44 -0700

    Update model.py

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import fit_the_thing as f
 
 class CreditModel:
     def __init__(self):
@@ -17,8 +17,8 @@ class CreditModel:
         You should somehow manipulate and store this data to your model class
         so that you can make predictions on new testing data later on.
         """
-
-        # TODO: Fit your model based on the given X and y.
+        
+        self.param = f.model(X_train,  y_train)
         pass
 
     def predict(self, X_test):
@@ -28,6 +28,6 @@ class CreditModel:
         You should make use of the data that you stored/computed in the
         fitting phase to make your prediction on this new testing data.
         """
-
+        return f.predict(self.param,X_test)
         # TODO: Predict on `X_test` based on what you learned in the fit phase.
-        return np.random.randint(2, size=len(X_test))
+
